ede012906e783f7c18aab565c09e316b8be9c61a
Simon Lance <sl484@exeter.ac.uk>
2020-11-12 23:10:57 +0000

    Added 'alts' to images. Adjusted title

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,19 @@ myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/latest_1024_0094.jpg') {
       myImage.setAttribute('src','images/latest_1024_0131.jpg');
+      myImage.setAttribute('alt', 'SDO:AIA_131Angstrom');
     } else if(mySrc === 'images/latest_1024_0131.jpg') {
       myImage.setAttribute('src','images/latest_1024_0171.jpg');
+      myImage.setAttribute('alt', 'SDO:AIA_171Angstrom');
     } else if(mySrc === 'images/latest_1024_0171.jpg') {
       myImage.setAttribute('src','images/latest_1024_0193.jpg');
+      myImage.setAttribute('alt', 'SDO:AIA_193Angstrom');
     } else if(mySrc === 'images/latest_1024_0193.jpg') {
       myImage.setAttribute('src','images/latest_1024_0304.jpg');
+      myImage.setAttribute('alt', 'SDO:AIA_304Angstrom');
     } else {
       myImage.setAttribute('src','images/latest_1024_0094.jpg');
+      myImage.setAttribute('alt', 'SDO:AIA_094Angstrom');
     }
 }
 
