e30c69920b05f1c1ef0f699fa4e5d26bad770e50
Simon Lance <sl484@exeter.ac.uk>
2020-11-12 23:50:21 +0000

    Added a second webpage & other misc. changes

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,7 @@ myImage.onclick = function() {
 }
 
 let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
+let myHeading = document.querySelector('h2');
 
 function setUserName(){
   let myName = prompt('Please enter your name.');
@@ -29,7 +29,7 @@ function setUserName(){
     setUserName();
   } else{
   localStorage.setItem('name', myName);
-  myHeading.textContent= 'Greetings  ' + myName + '!';
+  myHeading.textContent= 'Welcome  ' + myName + '!';
   }
 }
 
@@ -37,7 +37,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Greetings  ' + storedName + '!';
+  myHeading.textContent = 'Welcome  ' + storedName + '!';
 }
 
 myButton.onclick = function() {
